d27b00fb3f1069a1806a0e7f0cc4b9c03b4873b0
architg20 <architgnayak05@gmail.com>
2024-05-18 22:22:52 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import { Navbar } from './Layouts/NavBarAndFooter/NavBar';
+import { Footer } from './Layouts/NavBarAndFooter/Footer';
+import { HomePage } from './Layouts/HomePage/HomePage';
+
+
+export const  App = () => {
+  return (
+    <div >
+  <Navbar/>
+  <HomePage/>
+  <Footer/>
+    </div>
+  );
+}
+
+
